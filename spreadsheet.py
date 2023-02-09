7be1d2dfb4329507735f0006937cbee2391cddf6
ArtavazdGevorgyan <ag15012003@gmail.com>
2023-02-10 02:34:27 +0400

    Update spreadsheet.py

--- spreadsheet.py
+++ spreadsheet.py
@@ -1,6 +1,7 @@
 from cell import Cell
 import numpy as np
 
+
 class Spreadsheet:
     def __init__(self, row: int, col: int) -> None:
         self.spredsheet = np.empty(shape=(row, col), dtype=Cell)
@@ -15,19 +16,21 @@ class Spreadsheet:
         return self.spredsheet[row, col]
 
     def addRow(self, row: int) -> None:
-        self.spredsheet = np.insert(self.spredsheet, (row,), Cell, axis=0)
+        self.spredsheet = np.insert(self.spredsheet, (row,),
+                                    Cell(" "), axis=0)   # Fake a arac, chhavatal
 
     def removeRow(self, row: int) -> None:
         self.spredsheet = np.delete(self.spredsheet, (row,), axis=0)
 
     def addColumn(self, col: int) -> None:
-        self.spredsheet = np.insert(self.spredsheet, (col,), Cell, axis=1)
+        self.spredsheet = np.insert(self.spredsheet, (col,),
+                                    Cell(" "), axis=1)  # Nuynn el stex
 
-    def removeColumn(self, row: int) -> None:
-        self.spredsheet = np.delete(self.spredsheet, (1,), axis=1)
+    def removeColumn(self, col: int) -> None:
+        self.spredsheet = np.delete(self.spredsheet, (col,), axis=1)
 
     def swapRows(self, row1: int, row2: int) -> None:
-        self.spredsheet[[row1, row2]] = self.spredsheet[[row1, row2]]
+        self.spredsheet[[row1, row2]] = self.spredsheet[[row2, row1]]
 
     def swapColumns(self, col1: int, col2: int) -> None:
-        self.spredsheet[:, [col1, col2]] = self.spredsheet[:, [col1, col2]]
+        self.spredsheet[:, [col1, col2]] = self.spredsheet[:, [col2, col1]]
